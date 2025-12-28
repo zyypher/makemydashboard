093980d1f7fd19684219c25eb5309d3c9008a6f9
Vishnu Vinayan <therealvishnuvinayan@gmail.com>
2025-12-28 21:03:00 +0400

    google login working

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter: new PrismaNeon({
+      connectionString: process.env.DATABASE_URL!,
+    }),
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
